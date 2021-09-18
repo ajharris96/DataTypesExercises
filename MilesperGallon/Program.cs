1c43522de802857879138f4502c2268587a8986e
andrew <andrewjharris96@gmail.com>
2021-09-18 09:31:43 -0400

    mpg program done

---
+++ MilesperGallon/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MilesperGallon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many miles have you driven?");
+            double miles = double.Parse(Console.ReadLine());
+            Console.WriteLine("How many gallons of gas did you use?");
+            double gallons = double.Parse(Console.ReadLine());
+            double mpg = miles/gallons;
+            Console.WriteLine(mpg+ " miles per gallon");
+        }
+    }
+}
